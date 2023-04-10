8d3bf19a922f4f41d6e8ffa4f89e6888b3a1e6b8
mansooranis <mansooranis60@gmail.com>
2023-04-10 16:40:13 -0600

    ui changes

--- pages/Home.js
+++ pages/Home.js
@@ -1,7 +1,10 @@
 import Checkbox from 'expo-checkbox';
 import { Dialog, Input } from "@rneui/themed";
 import { useEffect, useState } from "react";
-import { Button, SafeAreaView, Text, TextInput, View } from "react-native";
+import { Button, Pressable, SafeAreaView, Text, TextInput, View } from "react-native";
+import { Icon } from 'react-native-elements'
+
+// import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 
 export default function Home() {
     const [habits, setHabits] = useState([]);
@@ -20,16 +23,18 @@ export default function Home() {
     return(
         <SafeAreaView className = "flex-1 flex-col bg-[#FFF8EA]">
             <Text className = "text-5xl font-bold self-start m-8">Habits</Text>
-            <View className = "h-[18%] bg-[#FFFBF4] border-solid border border-[#4F3C3D] shadow-sm shadow-[#BAAB90] ml-4 mr-4 mb-4 rounded-lg">
-
-            </View>
+            <Pressable className = "h-[18%] bg-[#FFFBF4] border-solid border border-[#4F3C3D] shadow-sm shadow-[#BAAB90] ml-4 mr-4 mb-4 rounded-lg">
+                <View className = "">
+                    
+                </View>
+            </Pressable>
             <View className = "border border-dashed ml-4 mr-4 rounded-lg border-[#4F3C3D]">
                 <Button title="Add a habit" color={"black"} onPress={() => {setAddHabitDialog(true)}}/>
             </View>
-            <View className="m-6">
+            <View className="m-6 flex flex-col space-y-3">
                 {habits.length === 0 && <Text className="text-lg">No habits added yet</Text>}
                 {habits.map((habit, index) => {return (
-                    <View key={index} className = "flex flex-row items-center bg-[#FFF8EA] gap-x-5">
+                    <View key={index} className = "flex flex-row items-center border p-4 rounded-md border-[#4F3C3D] bg-[#FFFBF4]">
                         <Checkbox onValueChange={
                             (checked) => {
                                 // console.log(checked);
@@ -44,7 +49,10 @@ export default function Home() {
                         value={
                             habit.checked
                         }/>
-                        <Button className="text-lg ml-3" title={habit.name}/>
+                        <Pressable className="flex-1 flex-row items-center justify-between">
+                            <Text className="text-xl ml-5">{habit.name}</Text>
+                            <Icon name="open-in-new" type="material" size={20} color="#4F3C3D" className=""/>
+                        </Pressable>
                     </View>
                 )})}
             </View>
